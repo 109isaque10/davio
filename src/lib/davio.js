d0cc71e2ce36ee78dcf055b511c587c6fa3c373a
Isaque Victor Duarte De Oliveira <45144815+109isaque10@users.noreply.github.com>
2024-12-19 01:11:36 -0300

    s

--- src/lib/davio.js
+++ src/lib/davio.js
@@ -88,6 +88,9 @@ async function getFiles(client, userConfig, type, name, season){
       files = files.filter(file => file.basename.includes(`Season ${numberPad(season)}`), file => file.basename.includes(`Season ${numberPad(season, 2)}`));
       files = await getFilesRecursive(client, files[0].filename);
       files.subtitles = files.filter(file => isSubtitle(file.basename));
+      if(files.subtitles === undefined){
+        files.subtitles = []
+      }
       files = files.filter(file => isVideo(file.basename));
       await cache.set(cacheKey, files, {ttl: 259200});
     }
