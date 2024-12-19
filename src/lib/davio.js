bd2461b170c129da3a4153ff8fd92f66228cbabd
Isaque Victor Duarte De Oliveira <45144815+109isaque10@users.noreply.github.com>
2024-12-19 00:46:04 -0300

    fix

--- src/lib/davio.js
+++ src/lib/davio.js
@@ -143,6 +143,7 @@ export async function getStreams(userConfig, type, stremioId, publicUrl){
     file.languages = config.languages.filter(l => parseWords(basename).join(' ').match(l.pattern));
     file.subtitles = []
     for (let index = 0; index < subtitles.length; index++) {
+      subtitle = subtitles[index];
       if((subtitle.basename.split('.').at(-1)).includes(file.basename)){
         file.subtitles.push(subtitle);
       }
